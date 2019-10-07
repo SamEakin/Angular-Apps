610ecf706719b4855ad9e968accfe81a99f3e64f
Sam Eakin <sseakin@gmail.com>
2019-10-07 11:15:09 -0500

    Initializing repo

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroService } from '../hero.service';
+import { Hero } from '../hero'; 
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) { }
+  
+  ngOnInit() {
+    this.getHeroes();
+  }
+}
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,24 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Tyler One' },
+  { id: 2, name: 'Asmond Gold' },
+  { id: 3, name: 'Doctor Disrespect' },
+  { id: 4, name: 'Niklas \'BizTalk\' LanMan' },
+  { id: 5, name: 'Temur' },
+  { id: 6, name: 'Ben Heyerdahl' },
+  { id: 7, name: 'Samuel Jones' },
+  { id: 8, name: 'Jacob Humphrey' },
+  { id: 9, name: 'Georgee Washington' },
+  { id: 10, name: 'Bob Tabor' },
+  { id: 11, name: 'Ram Palanichamy' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
